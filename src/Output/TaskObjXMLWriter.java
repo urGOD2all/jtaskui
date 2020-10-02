21168cacafee729d5c2901ccf79b42988d10486a
D.Weeks <urGOD2all@gmail.com>
2020-10-02 01:31:38 +0100

    Fixed up the code so that it will save all the Tasks and subtasks  On branch master 	modified:   src/Output/TaskObjXMLWriter.java

--- src/Output/TaskObjXMLWriter.java
+++ src/Output/TaskObjXMLWriter.java
@@ -91,31 +91,59 @@ public class TaskObjXMLWriter {
         root = null;
     }
 
-    private void buildDoc() {
-        Element e = null;
+    /**
+     * Recursive method that will take as TaskObj and check it for children (subtasks). If there are children
+     * then it will call itself with the first child.
+     *  If there are no children (a leaf) then it will add all the attributes for this child to the element
+     * and append the element to the root.
+     *
+     * @param TaskObj - task to add to the XML DOM objet. Should start with the root TaskObj
+     * @param Element - branch is the XML Element object that tasks in this call will add objects to. Starts as DOM root and each call should use its own Element
+     */
+    private void buildDoc(TaskObj task, Element branch) {
+        // Set this to be branch incase this is the first (root) call.
+        Element e = branch;
+        // Used to iterate children
+        int i = 0;
 
-        // Create an Element for this task
-        e = doc.createElement("task");
+        // If the task passed is the same as the rootTask then we skip it (because its not a Task so we don't save it, its just a holder for the top of the tree structure).
+        if(!task.equals(rootTask)) {
+            // Create an Element for this task
+            e = doc.createElement("task");
 
-        // TODO: This needs fixing, it will only look at the first child.
-        // Add all the attributes to the Task Element object
-        for(Map.Entry<String, String> attribute : rootTask.getChildAt(1).getAttributes().entrySet()) {
-            e.setAttribute(attribute.getKey(), attribute.getValue());
+            // Add all the attributes to this Element object
+            for(Map.Entry<String, String> attribute : task.getAttributes().entrySet()) {
+                e.setAttribute(attribute.getKey(), attribute.getValue());
+            }
+
+            // Check to see if we need to create a nested Element for the description
+            if(task.hasDescription()) {
+                // Create an Element for the description
+                Element desc = doc.createElement("description");
+                // Add the Task Description content
+                desc.setTextContent(task.getDescription());
+                // Add this description Element to this Task Element
+                e.appendChild(desc);
+            }
+
+            // Add this Task Element to the XML Element branch
+            branch.appendChild(e);
         }
-        // TODO: Do some checking and make sure there is a desciption to add
-        // Add the Task Description
-        Element desc = doc.createElement("description");
-        desc.setTextContent(rootTask.getChildAt(1).getDescription());
-        // Add this description Element to the Task Element
-        e.appendChild(desc);
 
-        // Add this Task Element to the Document root
-        root.appendChild(e);
+        // If this has children then we will have to get them and recurse
+        if(task.hasChildren()) {
+            while(i < task.getChildCount()) {
+                i = i + 1;
+                // Recurse with this child and use this Task XML element (e) as the branch
+                buildDoc(task.getChildAt(i), e);
+            }
+        }
+
     }
 
     public void write() {
         try {
-            buildDoc();
+            buildDoc(rootTask, root);
             transformer.transform(new DOMSource(doc), new StreamResult(file));
         }
         catch (Exception e) {
