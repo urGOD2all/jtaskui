17266afc534244214619803b12f43bcdeefa9c33
D.Weeks <urGOD2all@gmail.com>
2020-11-22 23:55:10 +0000

    Moved description tab code out to its own class.  On branch master 	new file:   src/jTaskEdit/descTab.java 	modified:   src/jTaskView/ui/jTaskEdit.java

---
+++ src/jTaskEdit/descTab.java
@@ -0,0 +1,90 @@
+package jTaskUI.jTaskEdit.tabs;
+
+import jtaskui.TaskObj;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+
+public class descTab {
+    private TaskObj task;
+
+    public descTab(TaskObj task) {
+        this.task = task;
+    }
+
+    public JPanel buildPanel() {
+        // Create the main panel that will get added to the tPane
+        JPanel descPanel = new JPanel(new BorderLayout(50, 20));
+
+        // West panel will contain the labels, Center panel will contain the data
+        BorderLayout westPanelLayout = new BorderLayout();
+        BorderLayout centerPanelLayout = new BorderLayout();
+        BorderLayout southPanelLayout = new BorderLayout();
+        // Set a small gap between the content on the panels (seperates the labels/text areas from each other with a small vertical gap)
+        JPanel westPanel = new JPanel(new BorderLayout(0, 10));
+        JPanel centerPanel = new JPanel(new BorderLayout(0, 10));
+        // The south panel will hold the same as above because more items are needed
+        JPanel southPanel = new JPanel(new BorderLayout(17, 10));
+        JPanel southWestPanel = new JPanel(new BorderLayout(0, 10));
+        JPanel southCenterPanel = new JPanel(new BorderLayout(0, 10));
+
+        // Populate the Description panel labels and data
+        // Subject
+        JLabel subjectLabel = new JLabel("Subject");
+        JTextArea subject = new JTextArea(task.getSubject());
+        // Description
+        JLabel descriptionLabel = new JLabel("Description");
+        JTextArea description = new JTextArea(task.getDescription());
+        description.setLineWrap(true);
+        JScrollPane descScroll = new JScrollPane(description);
+        // Priority
+        JLabel priorityLabel = new JLabel("Priority");
+        JSpinner priority = new JSpinner(new javax.swing.SpinnerNumberModel());
+        priority.setEditor(new javax.swing.JSpinner.NumberEditor(priority, "0"));
+        if(task.getPriority() != null) {
+            try {
+                priority.setValue(Integer.parseInt(task.getPriority()));
+            }   
+            catch(NumberFormatException e) {
+                priority.setValue(0);
+            }   
+        }   
+
+        // Creation date
+        JLabel creationDateLabel = new JLabel("Creation Date");
+        JLabel creationDate = new JLabel(task.getFormattedCreationDateTime());
+        // Modification date
+        JLabel modificationDateLabel = new JLabel("Modification Date");
+        JLabel modificationDate = new JLabel(task.getFormattedModificationDateTime());
+
+        // Add the labels and the data to the correct locations
+        westPanel.add(subjectLabel, BorderLayout.NORTH);
+        centerPanel.add(subject, BorderLayout.NORTH);
+        westPanel.add(descriptionLabel, BorderLayout.CENTER);
+        centerPanel.add(descScroll, BorderLayout.CENTER);
+        westPanel.add(southWestPanel, BorderLayout.SOUTH);
+        centerPanel.add(southCenterPanel, BorderLayout.SOUTH);
+        // SOUTH has a panel with two panels in it
+        southPanel.add(southWestPanel, BorderLayout.WEST);
+        southPanel.add(southCenterPanel, BorderLayout.CENTER);
+        // south NORTH reserved for priority
+        southWestPanel.add(priorityLabel, BorderLayout.NORTH);
+        southCenterPanel.add(priority, BorderLayout.NORTH);
+        southWestPanel.add(creationDateLabel, BorderLayout.CENTER);
+        southCenterPanel.add(creationDate, BorderLayout.CENTER);
+        southWestPanel.add(modificationDateLabel, BorderLayout.SOUTH);
+        southCenterPanel.add(modificationDate, BorderLayout.SOUTH);
+
+        // Add the west and center panels to the main panel
+        descPanel.add(westPanel, BorderLayout.WEST);
+        descPanel.add(centerPanel, BorderLayout.CENTER);
+        descPanel.add(southPanel, BorderLayout.SOUTH);
+
+        return descPanel;
+    }
+}
--- src/jTaskView/ui/jTaskEdit.java
+++ src/jTaskView/ui/jTaskEdit.java
@@ -1,6 +1,7 @@
 package jtaskview.ui;
 
 import jtaskui.TaskObj;
+import jTaskUI.jTaskEdit.tabs.descTab;
 import jTaskUI.jTaskEdit.tabs.datesTab;
 import jTaskUI.jTaskEdit.tabs.notesTab;
 
@@ -12,10 +13,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
 import javax.swing.JButton;
-import javax.swing.JSpinner;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -25,10 +23,7 @@ public class jTaskEdit implements ActionListener {
     private JFrame editFrame;
     private JTabbedPane tPane;
     private JButton closeEditFrame;
-    private JPanel editPanel, descPanel;
-    private JLabel subjectLabel, descriptionLabel, creationDateLabel, creationDate, modificationDateLabel, modificationDate;
-    private JTextArea subject, description;
-    private JScrollPane descScroll;
+    private JPanel editPanel;
 
     public jTaskEdit(TaskObj task) {
         // Store a reference to the TaskObj with all the information in it
@@ -68,7 +63,8 @@ public class jTaskEdit implements ActionListener {
         efSouthEast.add(closeEditFrame, BorderLayout.EAST);
 
         // Build each tab
-        initDescTab();
+        descTab dt = new descTab(task);
+        tPane.addTab("Description", dt.buildPanel());
         datesTab dat = new datesTab(task);
         tPane.addTab("Dates", dat.buildPanel());
 
@@ -86,82 +82,6 @@ public class jTaskEdit implements ActionListener {
         // Add components to the frames panel
         editPanel.add(tPane, BorderLayout.CENTER);
         editPanel.add(efSouth, BorderLayout.SOUTH);
-    }
-
-    /**
-     * Used internally to create the Description Tabbed Pane
-     * Uses BorderLayout because GridBagLayout makes a massive mess when resizing
-     */
-    private void initDescTab() {
-        // Create the main panel that will get added to the tPane
-        descPanel = new JPanel(new BorderLayout(50, 20));
-
-        // West panel will contain the labels, Center panel will contain the data
-        BorderLayout westPanelLayout = new BorderLayout();
-        BorderLayout centerPanelLayout = new BorderLayout();
-        BorderLayout southPanelLayout = new BorderLayout();
-        // Set a small gap between the content on the panels (seperates the labels/text areas from each other with a small vertical gap)
-        JPanel westPanel = new JPanel(new BorderLayout(0, 10));
-        JPanel centerPanel = new JPanel(new BorderLayout(0, 10));
-        // The south panel will hold the same as above because more items are needed
-        JPanel southPanel = new JPanel(new BorderLayout(17, 10));
-        JPanel southWestPanel = new JPanel(new BorderLayout(0, 10));
-        JPanel southCenterPanel = new JPanel(new BorderLayout(0, 10));
-
-        // Populate the Description panel labels and data
-        // Subject
-        subjectLabel = new JLabel("Subject");
-        subject = new JTextArea(task.getSubject());
-        // Description
-        descriptionLabel = new JLabel("Description");
-        description = new JTextArea(task.getDescription());
-        description.setLineWrap(true);
-        descScroll = new JScrollPane(description);
-        // Priority
-        JLabel priorityLabel = new JLabel("Priority");
-        JSpinner priority = new JSpinner(new javax.swing.SpinnerNumberModel());
-        priority.setEditor(new javax.swing.JSpinner.NumberEditor(priority, "0"));
-        if(task.getPriority() != null) {
-            try {
-                priority.setValue(Integer.parseInt(task.getPriority()));
-            }
-            catch(NumberFormatException e) {
-                priority.setValue(0);
-            }
-        }
-
-        // Creation date
-        creationDateLabel = new JLabel("Creation Date");
-        creationDate = new JLabel(task.getFormattedCreationDateTime());
-        // Modification date
-        modificationDateLabel = new JLabel("Modification Date");
-        modificationDate = new JLabel(task.getFormattedModificationDateTime());
-
-        // Add the labels and the data to the correct locations
-        westPanel.add(subjectLabel, BorderLayout.NORTH);
-        centerPanel.add(subject, BorderLayout.NORTH);
-        westPanel.add(descriptionLabel, BorderLayout.CENTER);
-        centerPanel.add(descScroll, BorderLayout.CENTER);
-        westPanel.add(southWestPanel, BorderLayout.SOUTH);
-        centerPanel.add(southCenterPanel, BorderLayout.SOUTH);
-        // SOUTH has a panel with two panels in it
-        southPanel.add(southWestPanel, BorderLayout.WEST);
-        southPanel.add(southCenterPanel, BorderLayout.CENTER);
-        // south NORTH reserved for priority
-        southWestPanel.add(priorityLabel, BorderLayout.NORTH);
-        southCenterPanel.add(priority, BorderLayout.NORTH);
-        southWestPanel.add(creationDateLabel, BorderLayout.CENTER);
-        southCenterPanel.add(creationDate, BorderLayout.CENTER);
-        southWestPanel.add(modificationDateLabel, BorderLayout.SOUTH);
-        southCenterPanel.add(modificationDate, BorderLayout.SOUTH);
-
-        // Add the west and center panels to the main panel
-        descPanel.add(westPanel, BorderLayout.WEST);
-        descPanel.add(centerPanel, BorderLayout.CENTER);
-        descPanel.add(southPanel, BorderLayout.SOUTH);
-
-        // Add the Description panel to the tPane
-        tPane.addTab("Description", descPanel);
     }
 
     public void actionPerformed(ActionEvent e) {
