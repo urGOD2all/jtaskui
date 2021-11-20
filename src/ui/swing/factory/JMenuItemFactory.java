ea174701356b1654128662974b268632f423f7f5
D.Weeks <urGOD2all@gmail.com>
2021-11-20 23:38:09 +0000

    Added a utility class to build JMenuItems that takes care of all the common tasks  On branch master 	new file:   src/ui/swing/factory/JMenuItemFactory.java

---
+++ src/ui/swing/factory/JMenuItemFactory.java
@@ -0,0 +1,37 @@
+package jtaskui.ui.swing.factory;
+
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+
+import javax.swing.KeyStroke;
+
+import java.awt.Dimension;
+
+public class JMenuItemFactory {
+    // TODO: need to add icon support
+    public static JMenuItem configureMenuItem(String title, int mnemonic, int accKeyCode, int accModifiers, ActionListener action, int width) {
+        JMenuItem item = configureMenuItem(title, mnemonic, accKeyCode, accModifiers, action);
+
+        item.setPreferredSize(new Dimension(width, item.getPreferredSize().height));
+
+        return item;
+    }   
+
+    public static JMenuItem configureMenuItem(String title, int mnemonic, int accKeyCode, int accModifiers, ActionListener action, boolean initialState) {
+        JMenuItem item = configureMenuItem(title, mnemonic, accKeyCode, accModifiers, action);
+
+        item.setEnabled(initialState);
+
+        return item;
+    }
+
+    public static JMenuItem configureMenuItem(String title, int mnemonic, int accKeyCode, int accModifiers, ActionListener action) {
+        JMenuItem item = new JMenuItem(title);
+
+        item.setMnemonic(mnemonic);
+        item.setAccelerator(KeyStroke.getKeyStroke(accKeyCode, accModifiers));
+        item.addActionListener(action);
+
+        return item;
+    }
+}
